b8d711c508990ec6bc90bce73df8777d07974d91
Lior Agnin <lior.agnin@gmail.com>
2024-01-09 14:54:13 +0200

    Add deprecation warning on EOASignature middleware

--- lib/src/preset/middleware/signature.dart
+++ lib/src/preset/middleware/signature.dart
@@ -2,7 +2,19 @@ import 'package:userop/src/types.dart';
 import 'package:web3dart/credentials.dart';
 import 'package:web3dart/crypto.dart';
 
+@Deprecated('eOASignature is deprecated. Replace with signUserOpHash.')
 UserOperationMiddlewareFn eOASignature(EthPrivateKey credentials) {
+  return (ctx) async {
+    ctx.op.signature = bytesToHex(
+      credentials.signPersonalMessageToUint8List(
+        ctx.getUserOpHash(),
+      ),
+      include0x: true,
+    );
+  };
+}
+
+UserOperationMiddlewareFn signUserOpHash(EthPrivateKey credentials) {
   return (ctx) async {
     ctx.op.signature = bytesToHex(
       credentials.signPersonalMessageToUint8List(
