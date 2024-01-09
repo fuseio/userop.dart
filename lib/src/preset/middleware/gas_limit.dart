f610a0274d1fb4a33d1e4fdde0f84eaa1dc07d5f
Lior Agnin <lior.agnin@gmail.com>
2024-01-09 14:53:10 +0200

    Remove redundant estimateCreationGas from gasLimit middleware

--- lib/src/preset/middleware/gas_limit.dart
+++ lib/src/preset/middleware/gas_limit.dart
@@ -1,33 +1,14 @@
 import 'package:userop/src/types.dart';
-import 'package:web3dart/crypto.dart';
 import 'package:web3dart/json_rpc.dart';
 import 'package:web3dart/web3dart.dart';
 
 import '../../models/index.dart';
-
-Future<BigInt> estimateCreationGas(
-  Web3Client client,
-  String initCode,
-) async {
-  final initCodeHex = initCode;
-  final factoryAddress = initCodeHex.substring(0, 42);
-  final callData = "0x${initCodeHex.substring(42)}";
-  return await client.estimateGas(
-    to: EthereumAddress.fromHex(factoryAddress),
-    data: hexToBytes(callData),
-  );
-}
 
 UserOperationMiddlewareFn estimateUserOperationGas(
   Web3Client client,
   RpcService provider,
 ) {
   return (ctx) async {
-    if (ctx.op.nonce.compareTo(BigInt.zero) == 0) {
-      ctx.op.verificationGasLimit = ctx.op.verificationGasLimit +
-          await estimateCreationGas(client, ctx.op.initCode);
-    }
-
     final rpcResponse = await provider.call(
       'eth_estimateUserOperationGas',
       [ctx.op.opToJson(), ctx.entryPoint.toString()],
